d64ddb0de679a54b63709a9cedab85616eef5485
Muzakkir <mdmuzakkir296@gmail.com>
2024-01-14 23:18:09 +0530

    Fix(App.tsx): colour changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <>
-      <div className="container dark:bg-slate-800">
-        <h1 className="text-3xl font-bold underline text-green-900">
+      <div className="dark:bg-slate-950">
+        <h1 className="text-red-900 dark:text-white text-3xl font-bold underline ">
           Hello world!!
         </h1>
       </div>
